88f2669762f2980b12b020b6932fa53b4334d26a
Rajib Chowdhury <rchowdhury@crri.co.in>
2012-03-28 16:11:05 +0530

    forms and layout midified.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,20 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  helper_method :home_link
+  
+  private
+  
+  # Overwriting the sign_in redirect path method
+  def after_sign_in_path_for(resource_or_scope)
+    props_path
+  end
+
+   # Overwriting the sign_out redirect path method
+   def after_sign_out_path_for(resource_or_scope)
+     root_path
+   end
+   
+   def home_link
+     user_signed_in? ? props_path : root_path
+   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Propapp::Application.routes.draw do
-  devise_for :users
+  devise_for :users do
+    root :to => 'devise/sessions#new'
+  end
 
   resources :props
 
@@ -52,7 +54,7 @@ Propapp::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'props#index'
+  # root :to => 'props#index'
 
   # See how all your routes lay out with "rake routes"
 
