ad2025242c3a8c678e6edf6f34c2e997add0e198
Rajib Chowdhury <rchowdhury@crri.co.in>
2012-03-30 15:04:50 +0530

    settings nav select fixed.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,9 @@
 module ApplicationHelper
-  def nav_class(path)
-    current_page?(path) ? "active" : ""
+  def nav_class(path, _controller=false)
+    if _controller
+      controller.controller_name == _controller ? "active" : ""
+    else
+      current_page?(path) ? "active" : ""
+    end
   end
 end
