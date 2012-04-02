5be5e512e09cb4071a7ee204a58e8c20ff340f1a
Rajib Chowdhury <chowdhury284@gmail.com>
2012-04-02 10:35:23 +0530

    category model added for prop.

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,8 +1,7 @@
-class Prop
+class Category
   include Mongoid::Document
   field :title, :type => String
   field :description, :type => String
-  field :category_id, :type => Integer
 
-  has_one :category
+  belongs_to :prop
 end
